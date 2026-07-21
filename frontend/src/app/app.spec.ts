60b5e01d67887c23d781c6c29e393a6f9998cade
sasa221 <saleh2005mohamed@gmail.com>
2026-07-22 01:23:11 +0300

    Implement frontend authentication

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,16 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
 import { App } from './app';
+import { AppModule } from './app-module';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        App
-      ],
+      imports: [AppModule],
     }).compileComponents();
   });
 
@@ -20,10 +15,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render login link', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
+    expect(compiled.querySelector('nav')?.textContent).toContain('Login');
   });
 });
---
+++ frontend/src/app/register/register.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Authservice } from '../Services/authservice';
+import { IUser } from '../models/iuser';
+
+@Component({
+  selector: 'app-register',
+  standalone: false,
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  user: IUser = {
+    username: '',
+    email: '',
+    password: '',
+  };
+  message = '';
+
+  constructor(private authservice: Authservice) {}
+
+  register(form: NgForm) {
+    if (form.valid) {
+      this.authservice.register(this.user).subscribe({
+        next: (data) => {
+          this.authservice.savedata(data);
+          this.message = 'Register Done';
+          form.resetForm();
+        },
+        error: (err) => {
+          this.message = err.error?.error || 'Register Failed';
+        },
+      });
+    }
+  }
+}
